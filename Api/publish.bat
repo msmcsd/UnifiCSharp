013cfd14abcb2bf94dc5983da85560d92d6f070a
Jason Shih <jason_shih@hotmail.com>
2022-11-22 17:39:47 -0800

    Server: Update publish.bat

--- Api/publish.bat
+++ Api/publish.bat
@@ -3,11 +3,11 @@ set ApiPublishFolder="\\vmware-host\Shared Folders\VMWare-Share\TestTools\Deploy
 if exist %ApiPublishFolder% rmdir %ApiPublishFolder% /s /q
 mkdir %ApiPublishFolder%
 dotnet publish -c Release -r win-x64 --no-self-contained -o %ApiPublishFolder%
-xcopy "\\vmware-host\Shared Folders\VMWare-Share\TestTools\Program\Unifi.json" %ApiPublishFolder% /s /e /y
+copy "\\vmware-host\Shared Folders\VMWare-Share\TestTools\Program\UnifiTasks.json" %ApiPublishFolder% /y
 
 REM 32 bit
 set ApiPublishFolder="\\vmware-host\Shared Folders\VMWare-Share\TestTools\Deploy\API\x86\"
 if exist %ApiPublishFolder% rmdir %ApiPublishFolder% /s /q
 mkdir %ApiPublishFolder%
 dotnet publish -c Release -r win-x86 --no-self-contained -o %ApiPublishFolder%
-xcopy "\\vmware-host\Shared Folders\VMWare-Share\TestTools\Program\Unifi.json" %ApiPublishFolder% /s /e /y
+copy "\\vmware-host\Shared Folders\VMWare-Share\TestTools\Program\UnifiTasks.json" %ApiPublishFolder% /y
