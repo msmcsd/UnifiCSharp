cd4be22ffbb5b51dc87a8c6cd908c311aa684141
Jason Shih <jason_shih@hotmail.com>
2022-12-08 16:43:28 -0800

    Update publish.bat

--- Api/publish.bat
+++ Api/publish.bat
@@ -9,5 +9,5 @@ REM 32 bit
 set ApiPublishFolder="\\vmware-host\Shared Folders\VMWare-Share\TestTools\Deploy\API\x86\"
 if exist %ApiPublishFolder% rmdir %ApiPublishFolder% /s /q
 mkdir %ApiPublishFolder%
-dotnet publish -c Release -r win-x86 --no-self-contained -o %ApiPublishFolder%
+dotnet publish C:\repos\UnifiCSharp\Api\UnifiApi.csproj -c Release -r win-x86 --no-self-contained -o %ApiPublishFolder%
 copy "\\vmware-host\Shared Folders\VMWare-Share\TestTools\Program\UnifiTasks.json" %ApiPublishFolder% /y
