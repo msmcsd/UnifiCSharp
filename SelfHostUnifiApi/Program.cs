d97534f307838ff7c097491deea331f073069a72
Jason Shih <jason_shih@hotmail.com>
2022-12-26 11:33:34 -0800

    SelfHostUnifiApi: Able to run as a service as well as from command line

--- SelfHostUnifiApi/Program.cs
+++ SelfHostUnifiApi/Program.cs
@@ -1,7 +1,4 @@
-﻿using System.Web.Http.SelfHost;
-using System.Web.Http;
-using System;
-using System.Web.Http.Cors;
+﻿using System;
 
 namespace SelfHostUnifiApi
 {
@@ -9,21 +6,15 @@ namespace SelfHostUnifiApi
     {
         public static void Main(string[] args)
         {
-            var apiUrl = "http://localhost:5000";
-
-            var config = new HttpSelfHostConfiguration(apiUrl);
-            config.Routes.MapHttpRoute("default",
-                                        "api/{controller}/{action}",
-                                        new { controller = "Commands", action = RouteParameter.Optional });
-
-            config.EnableCors(new EnableCorsAttribute("http://localhost:3000, https://msmcsd.github.io", headers: "*", methods: "*"));
-
-            var server = new HttpSelfHostServer(config);
-            var task = server.OpenAsync();
-            task.Wait();
-
-            Console.WriteLine($"Web API Server has started at {apiUrl}");
-            Console.ReadLine();
+            if (args.Length > 0 && args[0] == "-s")
+            {
+                System.ServiceProcess.ServiceBase.Run(new UnifiService());
+            }
+            else
+            {
+                UnifiService.StartApiServer();
+                Console.ReadLine();
+            }
         }
     }
 }
---
+++ SelfHostUnifiApi/UnifiService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ServiceProcess;
+using System.Web.Http.Cors;
+using System.Web.Http.SelfHost;
+using System.Web.Http;
+
+namespace SelfHostUnifiApi
+{
+    internal class UnifiService : ServiceBase
+    {
+
+        protected override void OnStart(string[] args)
+        {
+            StartApiServer();
+            base.OnStart(args);
+        }
+
+        public static void StartApiServer()
+        {
+            var apiUrl = "http://localhost:5000";
+
+            var config = new HttpSelfHostConfiguration(apiUrl);
+            config.Routes.MapHttpRoute("default",
+                                        "api/{controller}/{action}",
+                                        new { controller = "Commands", action = RouteParameter.Optional });
+
+            config.EnableCors(new EnableCorsAttribute("http://localhost:3000, https://msmcsd.github.io", headers: "*", methods: "*"));
+
+            var server = new HttpSelfHostServer(config);
+            var task = server.OpenAsync();
+            task.Wait();
+
+            Console.WriteLine($"Web API Server has started at {apiUrl}");
+        }
+    }
+}
